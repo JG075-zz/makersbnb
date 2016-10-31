b18263b1c88c8afce60ea6ba9c9438641109cf34
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-31 16:56:59 +0000

    took out annoying thing

--- models/data_mapper_setup.rb
+++ models/data_mapper_setup.rb
@@ -3,7 +3,6 @@ require 'dm-postgres-adapter'
 require_relative 'property'
 require_relative 'user'
 
-  DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
