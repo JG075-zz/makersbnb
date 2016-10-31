927e8892ef45ee8dab1d001b2e89a3ef16d92232
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-31 14:16:20 +0000

    made it work

--- models/data_mapper_setup.rb
+++ models/data_mapper_setup.rb
@@ -2,7 +2,6 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'property'
 
-
   DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
   DataMapper.finalize
