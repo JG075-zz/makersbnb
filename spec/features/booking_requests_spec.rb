e8d69b098595253334a056dcf97d49b3e1477e31
James Gardiner <jamesgardiner075@gmail.com>
2016-11-02 12:57:16 +0000

    Add show received requests feature

--- spec/features/booking_requests_spec.rb
+++ spec/features/booking_requests_spec.rb
@@ -1,42 +1,56 @@
 require_relative 'web_helper'
 
-feature "booking requests" do
-  scenario "users can make a booking" do
+feature "booking requests as a customer" do
+  before :each do
     sign_up
     Property.create(name: "super cool house", location: "47 cool lane", description: "a very cool house", price: 100, user_id: 1)
     visit '/spaces'
     click_button('Rent')
-    expect(page).to have_content "Thank you for your request"
-
   end
+
+  scenario "users can make a booking" do
+    expect(page).to have_content "Thank you for your request"
+  end
+
   scenario "users can view their own requested bookings" do
-    sign_up
-    Property.create(name: "super cool house", location: "47 cool lane", description: "a very cool house", price: 100, user_id: 1)
-    visit '/spaces'
-    click_button('Rent')
     click_button("Log out")
     sign_up2
     visit '/requests'
     expect(page).to have_content "All requests:"
     expect(page).not_to have_content "super cool house"
   end
+
   scenario "can accept bookings" do
-    sign_up
-    Property.create(name: "super cool house", location: "47 cool lane", description: "a very cool house", price: 100, user_id: 1)
-    visit '/spaces'
-    click_button('Rent')
     visit '/requests'
     click_button("Accept")
     expect(page).not_to have_content "super cool house"
   end
 
   scenario "can decline bookings" do
-    sign_up
-    Property.create(name: "super cool house", location: "47 cool lane", description: "a very cool house", price: 100, user_id: 1)
-    visit '/spaces'
-    click_button('Rent')
     visit '/requests'
     click_button("Decline")
     expect(page).not_to have_content "super cool house"
+  end
+end
+
+feature "booking requests as a renter" do
+  before :each do
+    sign_up
+    Property.create(name: "super cool house", location: "47 cool lane", description: "a very cool house", price: 100, user_id: 1)
+    visit '/spaces'
+    click_button("Log out")
+  end
+
+  scenario "can view requests I have received" do
+    sign_up2
+    visit '/spaces'
+    click_button('Rent')
+    click_button("Log out")
+    fill_in :email, with: "Li@gmail.com"
+    fill_in :password, with: "Secret"
+    click_button "Log in"
+    visit '/requests'
+    expect(page).to have_content 'Requests I\'ve Received'
+    expect(page).to have_content 'You have received booking request for super cool house'
   end
 end
