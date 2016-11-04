998cde5026fc368a82c9275d92c010b6989316a0
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-11-04 16:45:09 +0000

    merge errors

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -1,7 +1,9 @@
 class MakersBnb < Sinatra::Base
   post '/requests/new' do
     @property = params[:property]
-    Request.create(booker_id: current_user.id, property_id: @property, start_date: params[:start_rent], end_date: params[:end_rent])
+    start_rent = params[:start_rent]
+    end_rent = params[:end_rent]
+    Request.create(booker_id: current_user.id, property_id: @property, start_date: start_rent, end_date: end_rent)
     erb :'/requests/new'
   end
 
@@ -14,29 +16,36 @@ class MakersBnb < Sinatra::Base
 
   post '/accept' do
     property_id = params[:property]
-    rent = Request.get(params[:request])
-    chosen_dates = Dates.new
-    chosen_dates.get_all_dates((rent.start_date.to_s),(rent.end_date.to_s))
-    chosen_dates.create_filter_dates_array
-    @days = chosen_dates.days
-    @filter_dates = chosen_dates.filter_dates
-      if (Property.get(property_id).days & @days) == @days
-        @filter_dates.each do |day|
-          remove_day = Property.get(property_id).days(:conditions => {:date => day})
-          remove_day.destroy!
-          Property.get(property_id).days(:conditions => {:date => day}).each do |x|
-            Day.get(x.id).destroy!
-          end
-        end
-      Request.get(params[:request]).destroy!
-      redirect '/requests'
-    else
-      flash.now[:errors] = "Property has already been booked for these dates. Please decline."
-      @made_requests = Property.all.requests(booker_id: current_user.id)
-      user_properties = Property.all(user_id: current_user.id)
-      @received_requests = user_properties.all.requests
-      erb :'requests/index'
+    request_id = params[:request]
+    rent = Request.get(request_id)
+    filter_dates = []
+    days = []
+    (Date.parse(rent.start_date.to_s)..Date.parse(rent.end_date.to_s)).map(&:to_s).each do |day|
+      if Day.first(date: day).nil?
+        flash.now[:errors] = "Property has already been booked for these dates. Please decline."
+        @made_requests = Property.all.requests(booker_id: current_user.id)
+        user_properties = Property.all(user_id: current_user.id)
+        @received_requests = user_properties.all.requests
+        return erb :'requests/index'
+      else
+        days << Day.first(date: day)
+      end
     end
+    days.each do |y|
+      filter_dates << y.date
+    end
+      filter_dates.each do |day|
+        remove_day = Property.get(property_id).days(:conditions => {:date => day})
+        remove_day.destroy!
+        Property.get(property_id).days(:conditions => {:date => day}).each do |x|
+          Day.get(x.id).destroy!
+        end
+        if Property.get(property_id).days.empty?
+          Property.get(property_id).destroy!
+        end
+      end
+      Request.get(request_id).destroy!
+      redirect '/requests'
 end
 
   post '/decline' do
