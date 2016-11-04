62409606b82ea4c05675f48711f0d3e9217fb4f1
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-11-04 11:36:14 +0000

    refractored dates

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,20 +1,36 @@
-def filter_date(start, finish)
-  @properties = []
-  filter_dates = []
-  days = []
-  (Date.parse(start)..Date.parse(finish)).map(&:to_s).each do |day|
-    days << Day.first_or_create(date: day)
+class Dates
+  attr_reader :start, :finish, :filter_dates
+  attr_accessor :properties, :days
+
+  def initialize
+    @properties = []
+    @filter_dates = []
+    @days = []
+    @available_days = []
   end
-  days.each do |y|
-    filter_dates << y.date
-  end
-  Property.all.each do |property|
-    available_days = []
-    property.days.each do |x|
-      available_days << x.date
+
+  def get_all_dates(start, finish)
+    (Date.parse(start)..Date.parse(finish)).map(&:to_s).each do |day|
+      @days << Day.first_or_create(date: day)
     end
-    if (available_days & filter_dates) == filter_dates
-      @properties << property
+  end
+
+  def create_filter_dates_array
+    @days.each do |y|
+      @filter_dates << y.date
+    end
+  end
+
+  def check_for_availability(start, finish)
+    get_all_dates(start, finish)
+    create_filter_dates_array
+    Property.all.each do |property|
+      property.days.each do |x|
+        @available_days << x.date
+      end
+      if (@available_days & @filter_dates) == @filter_dates
+        @properties << property
+      end
     end
   end
 end
